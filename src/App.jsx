5879ebf85157e989fa9768e336724791723372e0
karim12345-gif <karim991996@gmail.com>
2021-11-21 15:34:46 +0400

    docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,43 @@
-import Topbar from "./components/topbar/Topbar"
-import Intro from "./components/intro/Intro"
-import Portfolio from "./components/portfolio/Portfolio"
-import Works from "./components/works/Works"
-import Contact from "./components/contact/Contact"
-import './app.scss'
-import { useState } from "react"
-import Menu from "./components/menu/Menu"
-import AboutMe from "./components/about/AboutMe"
-import Skills from "./components/skills/Skills"
-import Progress from "./components/progress/Progress"
-// import {MediaQuery} from 'react-responsive'
-
-
-
-
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
+import AboutMe from "./components/about/AboutMe";
+import Skills from "./components/skills/Skills";
+import Progress from "./components/progress/Progress";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [menuOpen,setMenuOpen] = useState(false);
- 
-
-
-
-  
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    
-    <div className="app">
+    <BrowserRouter>
+      <HashRouter basename="/">
+        <div className="app">
+          {/* for the hamburger button to change colors */}
+          <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          {/* for the hamburger to close*/}
+          <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <Progress />
 
-      {/* for the hamburger button to change colors */}
-      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      {/* for the hamburger to close*/}
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <Progress />
- 
+          <div className="sections">
+            {/* <MediaQuery minWidth={1224}  minResolution="2dppx"> */}
+            <Intro />
+            <Portfolio />
+            <AboutMe />
+            <Skills />
+            <Works />
+            <Contact />
 
-     
-
-      <div  className="sections" >
-        {/* <MediaQuery minWidth={1224}  minResolution="2dppx"> */}
-        <Intro/>
-        <Portfolio/>
-        <AboutMe/>
-        <Skills/>
-        <Works/>
-        <Contact/>
-  
-         {/* </MediaQuery>  */}
-      </div>
-
-      
-        
-    </div>
+            {/* </MediaQuery>  */}
+          </div>
+        </div>
+      </HashRouter>
+    </BrowserRouter>
   );
 }
 
